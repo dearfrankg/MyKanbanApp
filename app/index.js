005d8e459c762483e307648a03b94306a5ed276c
Frank Gutierrez <dearfrankg@gmail.com>
2016-03-30 12:31:12 -0700

    Note app with React/Redux: setup redux

--- app/index.js
+++ app/index.js
@@ -1,7 +1,28 @@
 import './main.css';
 
+import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
+import DevTools from './components/DevTools';
+import rootReducer from './dux'
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+const createStoreWithMiddleware = compose(
+  DevTools.instrument()
+)(createStore);
+
+const initialState = {}
+const store = createStoreWithMiddleware(rootReducer, initialState)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <App />
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById('app')
+);
