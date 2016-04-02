927ea6e55863cb532946492da8f8d87aa1e32744
Frank Gutierrez <dearfrankg@gmail.com>
2016-04-02 15:40:45 -0700

    Implementing drag-n-drop: Setting Up React DnD

--- app/components/App.js
+++ app/components/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux'
 import * as laneActions from '../dux/lanes'
 import Lanes from './Lanes'
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
+@DragDropContext(HTML5Backend)
 @connect((state) => ({
   lanes: state.lanes
 }), {
