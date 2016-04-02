cfcb1fef102bcb7f05d6e9b1a5fca9489c16c68f
Frank Gutierrez <dearfrankg@gmail.com>
2016-04-02 16:02:11 -0700

    Implementing drag-n-drop: Allowing Notes to Be Dragged

--- app/components/Note.js
+++ app/components/Note.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default class Note extends React.Component {
-  render() {
-    return <li {...this.props}>{this.props.children}</li>;
+
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin dragging note', props);
+
+    return {};
   }
+};
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+
+    console.log('dragging note', sourceProps, targetProps);
+  }
+};
+
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Note extends React.Component {
+
+  render() {
+    const {
+      connectDragSource,
+      connectDropTarget,
+      id,
+      onMove,
+      ...props} = this.props;
+
+    return connectDragSource(connectDropTarget(
+      <li {...props}>{props.children}</li>
+    ));
+  }
+
 }
